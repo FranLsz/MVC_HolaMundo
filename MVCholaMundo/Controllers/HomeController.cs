d94c4f5b9cd390f9ba815ff8e14a30f7ba0863fd
FranLsz <franlsz95develop@gmail.com>
2015-11-10 14:24:26 +0100

    first commit

---
+++ MVCholaMundo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using System.Web.Mvc;
+using MVCholaMundo.Models;
+
+namespace MVCholaMundo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Saludo> lista =
+            new List<Saludo>()
+            {
+                new Saludo() {Idioma = "Es", Texto = "Buenos dias"},
+                new Saludo() {Idioma = "En", Texto = "Good morning"},
+                new Saludo() {Idioma = "Fr", Texto = "Bon Jour"},
+                new Saludo() {Idioma = "De", Texto = "Guten morgen"},
+
+            };
+
+        // GET: Home
+        //Resultado de una operacion realizada (MVC no soporta la sobrecarga de metodos)
+        public ActionResult Index(String id)
+        {
+
+            var data = lista.FirstOrDefault(o => o.Idioma == id);
+
+            if (data == null)
+                data = lista.First(o => o.Idioma == "Es");
+
+            //data = lista.First(o => o.Idioma == "Es");
+
+            return View(data);
+        }
+    }
+}
---
+++ MVCholaMundo/Models/Saludo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCholaMundo.Models
+{
+    public class Saludo
+    {
+        public String Idioma { get; set; }
+        public String Texto { get; set; }
+
+    }
+}
